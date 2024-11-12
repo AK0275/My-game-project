c03c6cc96700ea2fa78b1aec517fd44a530b37fa
AK0275 <abdullakhalid0275@gmail.com>
2024-11-12 14:20:57 +0300

    project

---
+++ README.md
@@ -0,0 +1,13 @@
+as a user I should see letters buttom 
+
+as a user I want to choose a letter
+
+as a user I need to see a hang man 
+
+as a user I need to get the right word to win
+
+as a user I need to see the game looks good 
+
+the title  of the game is HangMan
+
+the user need to find the right letters to creat a word and if the user did not find the right letters the man will get hang 
---
+++ app.js
@@ -0,0 +1,31 @@
+//variables
+//give a variable for user
+//give a variable for hangman
+//give a variable for message
+
+
+//Elements
+//element for letter
+//element for button
+//element for text 
+
+
+//functions
+//const array for letters 
+//const arry for words
+//make a keyboard button
+//make function to give a random word
+//make function for right letters
+//make function for win 
+//make function for lose
+//make function for hide letters after choose
+//make function for creat a hangman body 
+//make function to reset the game if the user win or lose
+
+
+
+//eventListener
+
+
+
+
