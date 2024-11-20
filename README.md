6769cb36ca188aeb5764d5db00727f9938e051a2
AK0275 <abdullakhalid0275@gmail.com>
2024-11-20 21:44:18 +0300

    project1

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ as a user I need to get the right word to win
 
 as a user I need to see the game looks good 
 
-the title  of the game is HangMan
+the title of the game is HangMan
 
 the user need to find the right letters to creat a word and if the user did not find the right letters the man will get hang 
+
--- app.js
+++ app.js
@@ -1,31 +1,115 @@
-//variables
-//give a variable for user
-//give a variable for hangman
-//give a variable for message
+const randomAnimalsNames = ["Dog", "Cat", "Lion", "Rat", "Tiger", "Bear", "Monkey", "Wolf", "Fox", "Pig", "Cow", "Goat", "Horse", "Donkey", "Camel", "Zebra", "Elk", "Bison", "Panther", "Mole", "Duck", "Snake", "Pony"]
 
+let totalChances = 0;
 
-//Elements
-//element for letter
-//element for button
-//element for text 
+const hangStandChildren = document.querySelector(".hangstand").children
 
+for (let index = 0; index < hangStandChildren.length; index++) {
 
-//functions
-//const array for letters 
-//const arry for words
-//make a keyboard button
-//make function to give a random word
-//make function for right letters
-//make function for win 
-//make function for lose
-//make function for hide letters after choose
-//make function for creat a hangman body 
-//make function to reset the game if the user win or lose
+const element = hangStandChildren[index]
 
+element.classList.add("display-none")
+}
 
+let randomAnimalName = null
 
-//eventListener
+const getRandomNumber = (min, max) => {
+return Math.trunc(Math.random() * (max - min) + min)
+}
 
+const giveMeButtonsOnScreen = () => {
+const rootEl = document.querySelector(".buttons-parent")
 
+let buttonsDataArray = Array(26).fill(null)
 
+let i = 65;
 
+buttonsDataArray = buttonsDataArray.map((value) => {
+return String.fromCharCode(i++)
+});
+
+buttonsDataArray.forEach((el) => {
+const btn = document.createElement("button")
+btn.textContent = el;
+rootEl.appendChild(btn)
+});
+}
+
+const chooseRandomAnimalName=() => {
+
+const blankParentEl = document.querySelector(".blanks_parent")
+
+const randomNumber = getRandomNumber(0, randomAnimalsNames.length)
+
+randomAnimalName = randomAnimalsNames[randomNumber].toUpperCase()
+
+for (let index = 0; index < randomAnimalName.length; index++) {
+
+const letter = randomAnimalName[index]
+
+const alpha = document.createElement("p")
+
+const para = document.createElement("span")
+
+para.textContent = letter;
+
+alpha.appendChild(para)
+
+blankParentEl.appendChild(alpha)
+}
+}
+
+giveMeButtonsOnScreen()
+chooseRandomAnimalName()
+
+const buttonsParentEl = document.querySelector(".buttons-parent")
+
+let checkStatusGlobal = 0;
+
+const buttonParentClickFunction = function (e) {
+
+let checkStatus = 0;
+
+if (e.target.textContent.length === 1) {
+const letterClicked = e.target.textContent
+
+console.log(randomAnimalName)
+
+const allLettersEls = document.getElementsByTagName("span")
+
+for (let index = 0; index < allLettersEls.length; index++) {
+const spanEl = allLettersEls[index]
+
+if (letterClicked == spanEl.textContent) {
+
+spanEl.parentElement.textContent = letterClicked
+
+checkStatus++
+checkStatusGlobal++
+}
+}
+
+if (checkStatus != 0) {
+e.target.classList.add("greenBtn")
+} else {
+totalChances++;
+const elementToRemoveClass = document.querySelector(`.class-${totalChances}`)
+elementToRemoveClass.classList.remove("display-none")
+e.target.classList.add("redBtn")
+}
+
+e.target.setAttribute("disabled", "disabled")
+}
+
+if (totalChances == 10) {
+alert("You Lost the Game")
+location.reload()
+}
+
+if (checkStatusGlobal == randomAnimalName.length) {
+alert("You won the Game")
+location.reload()
+}
+};
+
+buttonsParentEl.addEventListener("click", buttonParentClickFunction)
